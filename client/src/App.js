98ebcffaa9e8e90e7f67c4618ad09975ef4b1214
ShishirShekhar <sspdav02@gmail.com>
2023-08-10 15:43:27 +0530

    removed header section in app

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Import required pages
 import Home from "./pages/Home";
-import Rooms from "./pages/Rooms"
+import Rooms from "./pages/Rooms";
 import Room from "./pages/Room";
-import NotFound from "./pages/NotFound"
+import NotFound from "./pages/NotFound";
 // Import css
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Router>
-          <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/rooms" exact element={<Rooms />} />
-            <Route path="/room" exact element={<Room />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Router>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/rooms" exact element={<Rooms />} />
+          <Route path="/room" exact element={<Room />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
