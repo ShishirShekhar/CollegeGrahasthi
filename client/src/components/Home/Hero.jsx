61dd4a6600c58dfa57be3f1e107a3482e4ea8727
ShishirShekhar <sspdav02@gmail.com>
2023-08-17 15:05:09 +0530

    added details to hero section

--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -1,5 +1,5 @@
 // Import required modules
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 // Import required components
 import Nav from "../Global/Nav";
 // Import required images
@@ -7,8 +7,36 @@ import hero from "../../assets/hero.png";
 
 const Hero = () => {
   return (
-    <Box className="section" sx={{ background: `url(${hero})`}}>
+    <Box
+      sx={{
+        background: `url(${hero})`,
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "85vh",
+      }}
+    >
       <Nav />
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "white",
+          padding: "2rem 1rem",
+        }}
+      >
+        <Typography variant="h3" className="title">
+          All your accommodation needs at one place.
+        </Typography>
+
+        <Typography variant="subtitle1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem animi
+          sequi voluptatum veritatis excepturi ducimus minus doloribus beatae
+          tempora dolore.
+        </Typography>
+      </Box>
     </Box>
   );
 };
